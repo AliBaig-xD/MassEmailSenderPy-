d9f2dc684e61d1c866d1d26b5b26ada4ac65b4b1
AliBaig-xD <69807734+AliBaig-xD@users.noreply.github.com>
2020-09-09 07:05:03 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Configuration file for smtp server
+
+smtp_server = dict()
+
+smtp_server['smtp_username'] = 'test@user.com'            # Enter your smtp username
+smtp_server['smtp_password'] = 'Password123!'    # Enter your smtp password
+smtp_server['smtp_host'] = 'smtp.testuser.com'                     # Enter your smtp host
+smtp_server['smtp_port'] = 465                                  # Enter your smtp port
+
+
